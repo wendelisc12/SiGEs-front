6e33039020e1f435114cf9e989d24d81fdcfc022
Wendel <wendel.isaac12@gmail.com>
2024-01-15 22:36:57 -0300

    feat: criando pagina de financeiro

---
+++ src/app/Pages/financeiro-page/financeiro-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financeiro-page',
+  templateUrl: './financeiro-page.component.html',
+  styleUrls: ['./financeiro-page.component.css']
+})
+export class FinanceiroPageComponent {
+
+}
