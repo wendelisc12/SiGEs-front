f4345eb4e4a98466e4e5361df3e1f94fb1866fe4
Wendel <wendel.isaac12@gmail.com>
2024-01-15 22:35:03 -0300

    feat: configurando menu sidebar

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  @ViewChild(MatMenuTrigger) alunoMenuTrigger?: MatMenuTrigger;
+  showAlunoMenu: boolean = false;
+  alunosActive: boolean = false;
 
-  
+  toggleAlunoMenu() {
+    if (this.alunoMenuTrigger) {
+      this.showAlunoMenu = !this.showAlunoMenu;
+      this.alunosActive = !this.alunosActive;
+
+      this.alunoMenuTrigger.menuClosed.subscribe(() => {
+        this.showAlunoMenu = false;
+        this.alunosActive = false;
+      });
+    }
+  }
 }
