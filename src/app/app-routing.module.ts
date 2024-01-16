256ced09a022c1037f05edf2778345dcbea5134e
Wendel <wendel.isaac12@gmail.com>
2024-01-15 22:27:23 -0300

    fix: arrumando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdicionarAlunoComponent } from './Components/Adicionar-alunos/adicionar-alunos.component';
 import { AdicionarAlunosPageComponent } from './Pages/adicionar-alunos-page/adicionar-alunos-page.component';
 import { InicioPageComponent } from './Pages/inicio-page/inicio-page.component';
+import { FinanceiroPageComponent } from './Pages/financeiro-page/financeiro-page.component';
+import { DocumentacaoPageComponent } from './Pages/documentacao-page/documentacao-page.component';
+import { ConsultarAlunoPageComponent } from './Pages/consultar-aluno-page/consultar-aluno-page.component';
 
 const routes: Routes = [
- 
-
-  { path: 'adicionar-alunos', component: AdicionarAlunoComponent },
-  { path: 'login', redirectTo: 'login', pathMatch: 'full' },  
-  { path: 'adicionar-alunos-page', component: AdicionarAlunosPageComponent },
-  { path: 'inicio-page', component: InicioPageComponent}
+  { path: 'aluno/adicionar', component: AdicionarAlunosPageComponent },
+  { path: 'aluno/consultar', component: ConsultarAlunoPageComponent },
+  { path: 'adicionarAluno', component: AdicionarAlunosPageComponent },
+  { path: 'financeiro', component: FinanceiroPageComponent },
+  { path: 'documentacao', component: DocumentacaoPageComponent },
+  { path: '', component: InicioPageComponent}
 ];
 
 @NgModule({
