317d13a0688dfd2652853948648abb728cc49e54
Wendel <wendel.isaac12@gmail.com>
2024-01-15 22:31:53 -0300

    fix: configurando imports do modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { AdicionarAlunoComponent } from './Components/Adicionar-alunos/adicionar-alunos.component';
-import { LoginComponent } from './Components/login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AdicionarAlunosPageComponent } from './Pages/adicionar-alunos-page/adicionar-alunos-page.component';
-import { InicioComponent } from './Components/inicio/inicio.component';
 import { InicioPageComponent } from './Pages/inicio-page/inicio-page.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-
-
-
+import { FinanceiroPageComponent } from './Pages/financeiro-page/financeiro-page.component';
+import { DocumentacaoPageComponent } from './Pages/documentacao-page/documentacao-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,AdicionarAlunoComponent, LoginComponent,AdicionarAlunosPageComponent, InicioComponent,InicioPageComponent, SidebarComponent
+    AppComponent, AdicionarAlunosPageComponent, InicioPageComponent, SidebarComponent, FinanceiroPageComponent, DocumentacaoPageComponent,
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,AppRoutingModule, HttpClientModule,FormsModule,CommonModule, MatDialogModule, BrowserAnimationsModule, MatSidenavModule, MatListModule ],
+    MatFormFieldModule, 
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule, 
+    MatSelectModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule, 
+    AppRoutingModule, 
+    HttpClientModule, 
+    FormsModule, 
+    CommonModule, 
+    MatDialogModule, 
+    BrowserAnimationsModule, 
+    MatSidenavModule, 
+    MatListModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
