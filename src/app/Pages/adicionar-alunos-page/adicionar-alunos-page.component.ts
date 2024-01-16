f9c3c47542d5c0c4476b4650289ff2a26b97ccdc
Wendel <wendel.isaac12@gmail.com>
2024-01-15 22:35:43 -0300

    feat: criando a pagina adicionar aluno

--- src/app/Pages/adicionar-alunos-page/adicionar-alunos-page.component.ts
+++ src/app/Pages/adicionar-alunos-page/adicionar-alunos-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-adicionar-alunos-page',
   templateUrl: './adicionar-alunos-page.component.html',
-  styleUrls: ['./adicionar-alunos-page.component.css'] // Adicione se desejar estilos específicos para este componente
+  styleUrls: ['./adicionar-alunos-page.component.css'],
 })
 export class AdicionarAlunosPageComponent {
- 
+  selectedDate: Date | null = null; // Inicializando a propriedade com null
+
+  // Método chamado quando a data é selecionada no datepicker
+  dateChanged(event: MatDatepickerInputEvent<Date, unknown>) {
+    this.selectedDate = event.value;
+  }
 }
