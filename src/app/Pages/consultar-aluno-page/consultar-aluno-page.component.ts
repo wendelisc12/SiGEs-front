1e9707a0d2eda5f0ef1e9846327828c95e3d7d1b
Wendel <wendel.isaac12@gmail.com>
2024-01-15 22:36:07 -0300

    feat: adicionando pagina consultar aluno

---
+++ src/app/Pages/consultar-aluno-page/consultar-aluno-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-aluno-page',
+  templateUrl: './consultar-aluno-page.component.html',
+  styleUrls: ['./consultar-aluno-page.component.css']
+})
+export class ConsultarAlunoPageComponent {
+
+}
