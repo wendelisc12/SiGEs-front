93fdd619d4d474bcf85ca1b10d9ccea663faa8eb
Wendel <wendel.isaac12@gmail.com>
2024-01-30 21:16:41 -0300

    feat: vizualizar aluno de acordo com id funcionando

---
+++ src/app/Pages/vizualizar-aluno-page/vizualizar-aluno-page.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EscolaService } from '../../services/escola.service';
+import { detalhesAluno } from '../../features/detalhesAlunoInterface';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-vizualizar-aluno-page',
+  templateUrl: './vizualizar-aluno-page.component.html',
+  styleUrls: ['./vizualizar-aluno-page.component.css']
+})
+export class VizualizarAlunoPageComponent {
+  id: string = '';
+  aluno:detalhesAluno = {matricula: '',nome_aluno: '',nome_responsavel: '',data_nascimento: '',cpf_responsavel: '',identidade_aluno: '',telefone: '',horario: '',serie: '',endereco: '',cep: '',data_pagamento: '',valor_mensalidade: ''}
+  isModalOpen = false;
+
+  constructor(private router: Router,private route: ActivatedRoute, private escolaService: EscolaService) { }
+
+
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    id ? this.id = id : this.id = ""
+    
+    this.getAluno()
+    
+  }
+
+  getAluno():void{
+    this.escolaService.getAluno(this.id).subscribe(
+      (data) => {
+        this.aluno = data
+        console.log(this.aluno)
+      },
+      (error) => {
+        console.error('Erro ao buscar alunos:', error);
+      })
+  }
+
+  onDeleteClick(): void {
+    this.isModalOpen = true;
+  }
+  get modalClass(): string {
+    return this.isModalOpen ? 'modal open' : 'modal';
+  }
+  
+  onConfirmDelete(): void {
+    this.isModalOpen = false;
+    this.escolaService.deleteAluno(this.id).subscribe(
+      (data) => {
+        this.router.navigate(['aluno/consultar']);
+        Swal.fire('Sucesso!', 'Aluno excluído com sucesso!', 'success');
+      },
+      (error) => {
+        console.error('Erro ao excluir aluno:', error);
+      }
+    );
+  }
+  
+  
+  onCancelDelete(): void {
+    this.isModalOpen = false;
+  }
+
+
+}
