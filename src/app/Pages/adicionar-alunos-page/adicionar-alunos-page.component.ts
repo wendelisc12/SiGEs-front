a3bbae5cf6080c0c601a05e29515dbe8ba17bd62
Wendel <wendel.isaac12@gmail.com>
2024-01-30 21:17:21 -0300

    feat: ajustando a função adicionar aluno e criando classe pra ele

--- src/app/Pages/adicionar-alunos-page/adicionar-alunos-page.component.ts
+++ src/app/Pages/adicionar-alunos-page/adicionar-alunos-page.component.ts
@@ -1,25 +1,87 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { ActivatedRoute, Router } from '@angular/router';
 import * as moment from 'moment';
+import { Aluno } from 'src/app/model/aluno.model';
+import Swal from 'sweetalert2';
+import { EscolaService } from 'src/app/services/escola.service';
 
 @Component({
   selector: 'app-adicionar-alunos-page',
   templateUrl: './adicionar-alunos-page.component.html',
   styleUrls: ['./adicionar-alunos-page.component.css'],
 })
+
 export class AdicionarAlunosPageComponent {
+
+  aluno:Aluno = new Aluno()
+
+  constructor(private router: Router, private escolaService:EscolaService) {}
+
   selectedDate: Date | null = null;
   dateChanged(event: MatDatepickerInputEvent<Date, unknown>) {
     this.selectedDate = event.value;
   }
 
-  onDateChange(event: MatDatepickerInputEvent<Date>) {
+
+  onDateChange(event: MatDatepickerInputEvent<Date>, campoData: string): void {
     if (event.value) {
       let date: Date = event.value;
       let formattedDate = moment(date).format('DD/MM/YYYY');
-      console.log(formattedDate);
+
+      if (campoData === 'nascimento') {
+        this.aluno.data_nascimento = formattedDate
+      } else if (campoData === 'pagamento') {
+        this.aluno.data_pagamento = formattedDate
+      }
     } else {
       console.log('Nenhuma data selecionada');
     }
   }
+
+  onTurnoChange(event: any): void {
+    this.aluno.horario = event.value;
+  }
+
+  gerarMatricula(): void {
+    const anoAtual = new Date().getFullYear();
+    this.aluno.matricula = `MAT${Math.floor(1000 + Math.random() * 9000).toString()}-${anoAtual}`;
+  }
+
+  adicionarAluno(form: NgForm): void { 
+    if (form.valid) {
+      this.gerarMatricula()
+      console.log(this.aluno)
+
+      const novoAluno = {
+        matricula: this.aluno.matricula,
+        nome_aluno: this.aluno.nome_aluno,
+        nome_responsavel: this.aluno.nome_responsavel,
+        data_nascimento: this.aluno.data_nascimento,
+        serie: this.aluno.serie,
+        data_pagamento: this.aluno.data_pagamento,
+        cpf_responsavel: this.aluno.cpf_responsavel,
+        telefone: this.aluno.telefone,
+        endereco: this.aluno.endereco,
+        cep: this.aluno.cep,
+        identidade_aluno: this.aluno.identidade_aluno,
+        horario: this.aluno.horario,
+        valor_mensalidade: this.aluno.valor_mensalidade,
+      };
+
+      this.escolaService.addAluno(novoAluno).subscribe(
+        (response) => {          
+          this.router.navigate(['aluno/consultar']);
+          Swal.fire('Sucesso!', 'Aluno adicionado com sucesso!', 'success');  
+        },
+        (error) => {
+          console.error('Erro ao adicionar aluno:', error);
+        }
+      );
+      console.log('Dados o Aluno:', this.aluno);
+
+    }
+  }
+
 }
---
+++ src/app/model/aluno.model.ts
@@ -0,0 +1,33 @@
+
+export class Aluno {
+    matricula: string;
+    nome_aluno: string;
+    nome_responsavel: string;
+    data_nascimento: string;
+    cpf_responsavel: string;
+    identidade_aluno: string;
+    telefone: string;
+    horario: string;
+    serie: string;
+    endereco: string;
+    cep: string;
+    data_pagamento: string;
+    valor_mensalidade: string;
+  
+    constructor() {
+      this.matricula = '';
+      this.nome_aluno = '';
+      this.nome_responsavel = '';
+      this.data_nascimento = '';
+      this.cpf_responsavel = '';
+      this.identidade_aluno = '';
+      this.telefone = '';
+      this.horario = '';
+      this.serie = '';
+      this.endereco = '';
+      this.cep = '';
+      this.data_pagamento = '';
+      this.valor_mensalidade = '';
+    }
+  }
+  
