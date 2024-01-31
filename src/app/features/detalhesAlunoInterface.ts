46719122c63aa69d98f994f6bcd30a74fbc77eb9
Wendel <wendel.isaac12@gmail.com>
2024-01-30 21:17:43 -0300

    feat: interface para detalhes do aluno

---
+++ src/app/features/detalhesAlunoInterface.ts
@@ -0,0 +1,15 @@
+export interface detalhesAluno{
+    matricula:string,
+    nome_aluno:string,
+    nome_responsavel:string,
+    data_nascimento:string,
+    cpf_responsavel:string,
+    identidade_aluno:string,
+    telefone:string,
+    horario:string,
+    serie:string,
+    endereco:string,
+    cep:string,
+    data_pagamento:string,
+    valor_mensalidade:string
+}
