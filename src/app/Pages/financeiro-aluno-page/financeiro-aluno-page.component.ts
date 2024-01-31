ca53d8bca723a91af1cba51c1336c79c36fbe8b7
Wendel <wendel.isaac12@gmail.com>
2024-01-30 22:41:30 -0300

    feat: pagina de detalhes financeiros do aluno

---
+++ src/app/Pages/financeiro-aluno-page/financeiro-aluno-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financeiro-aluno-page',
+  templateUrl: './financeiro-aluno-page.component.html',
+  styleUrls: ['./financeiro-aluno-page.component.css']
+})
+export class FinanceiroAlunoPageComponent {
+
+}
